4a081260d8580054528615a2384ec6d9a9cbc78e
jpoly1219 <jpoly@umich.edu>
2024-12-23 23:48:43 -0500

    change path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://chatlsp.org',
-  site: "https://hazel.org/chatlsp-site.github.io/",
+  // site: "https://hazel.org/chatlsp-site.github.io/",
+  site: "https://chatlsp.github.io/",
 });
